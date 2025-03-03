db928b4f1334bd40a25c7481c4d1ec1a17e4674b
Elsa Catoire <elsa.catoire@gmail.com>
2025-03-03 16:22:28 +0100

    add new pages for Autoclaves, IndusAutomatisme, IndusInformatique, and Machines; implement structure and content for improved industry representation

---
+++ microset/src/pages/Industrie/Autoclaves.tsx
@@ -0,0 +1,50 @@
+import HighlightCard from "../../components/HighlightCard";
+import MainCard from "../../components/MainCard";
+import MainPage from "../../components/MainPage";
+
+const Autoclaves = () => {
+  const presentation = {
+    title: "Industrie",
+    subtitle: "Autoclaves",
+    content: "Pilotage, automatisme et informatique d'autoclaves pour composites."
+}
+  const cards = [
+    {
+      title: "Nos compétences",
+      subtitle: "Hardware et software",
+      content: [
+        "Mise en place de systèmes",
+        "Réseaux",
+        "Progiciels de pilotage",
+      ],
+    },
+  ];
+
+  const references = {
+    title: "Nos références",
+    categories: [
+      {
+		title: "Aéronautique",
+        items: ["EADS", "AIRBUS", "Latécoère", "Eurocopter", "Letov", "Corse Composite", "IUT Toulouse"],
+      },
+    ],
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row gap-6 p-6 mx-auto">
+      <div className="flex-1">
+      <MainCard
+          title={presentation.title}
+          subtitle={presentation.subtitle}
+          content={presentation.content}
+        />
+        <MainPage cards={cards} />
+      </div>
+      <div className="w-fit">
+        <HighlightCard {...references} />
+      </div>
+    </div>
+  );
+};
+
+export default Autoclaves;
---
+++ microset/src/pages/Industrie/IndusInformatique.tsx
@@ -0,0 +1,75 @@
+import HighlightCard from "../../components/HighlightCard";
+import MainCard from "../../components/MainCard";
+import MainPage from "../../components/MainPage";
+
+const IndusInformatique = () => {
+  const presentation = {
+    title: "Industrie",
+    subtitle: "Informatique",
+    content: "WinProcess est un progiciel destiné au pilotage d'autoclaves et d'étuves (programmation, contrôle, acquisition de mesures de pression, de température et de vide)."
+}
+  const cards = [
+    {
+		id: 1,
+      title: "WINPROCESS : logiciel de pilotage d'autoclaves",
+      subtitle: "WinProcess est une application destinée à la conduite d'autoclaves.",
+      content: [
+        "Pilotage",
+        "Régulation",
+        "Enregistrement",
+        "Surveillance",
+        "Contrôle",
+        "Fabrication",
+        "Câblage petite / moyenne et grosse puissance",
+        "Maintenance",
+        "Schématique électrique",
+        "Schématique mécanique",
+      ],
+    },
+    {
+		id: 2,
+      title: "Supervision",
+      subtitle: "Synoptiques de réalisation industrielle.",
+      content:"",
+    },
+    {
+    id: 3,
+      title: "Nos compétences",
+      subtitle: "Hardware et software",
+      content: [
+        "Mise en place de systèmes",
+        "Réseaux",
+        "Progiciels de pilotage",
+      ],
+    },
+  ];
+
+  const references = {
+    title: "Nos références",
+    categories: [
+      {
+        id: 1,
+		title: "Aéronautique",
+        items: ["EADS", "AIRBUS", "Latécoère", "Eurocopter", "Letov", "Corse Composite", "IUT Toulouse"],
+      },
+    ],
+  };
+
+  return (
+    <div className="flex flex-col md:flex-row gap-6 p-6 mx-auto">
+      <div className="flex-1">
+      <MainCard
+          title={presentation.title}
+          subtitle={presentation.subtitle}
+          content={presentation.content}
+        />
+        <MainPage cards={cards} />
+      </div>
+      <div className="w-fit">
+        <HighlightCard {...references} />
+      </div>
+    </div>
+  );
+};
+
+export default IndusInformatique;
